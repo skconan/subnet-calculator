e739df8911e463f27c98bba3525a00dffae5b0b8
skconan <supakit.kr@gmail.com>
2017-11-21 03:36:21 -0800

    fix choice feild validation

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,16 +1,13 @@
 from django import forms
 from django.core.validators import RegexValidator
-from django.forms.fields import ChoiceField
 
-NETWORK_CLASS_CHOICES = (('default', 'Please Select a network class'), ('Any', 'Any'), ('A', 'A'), ('B', 'B'), ('C', 'C'),)
+NETWORK_CLASS_CHOICES = (('default', 'Please Select a network class'),
+                         ('Any', 'Any'), ('A', 'A'), ('B', 'B'), ('C', 'C'),)
 
-class ChoiceFieldNoValidation(ChoiceField):
-    def validate(self, value):
-        pass
-        
+
 class CalculatorForm(forms.Form):
-    network_class = ChoiceFieldNoValidation(choices=NETWORK_CLASS_CHOICES)
-    subnet = ChoiceFieldNoValidation(choices=())
+    network_class = forms.ChoiceField(choices=NETWORK_CLASS_CHOICES)
+    subnet = forms.ChoiceField(choices=())
     ip_address = forms.CharField(initial='0.0.0.0', validators=[RegexValidator(
         regex=r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
         message='IP Address is not a valid.'
@@ -19,3 +16,9 @@ class CalculatorForm(forms.Form):
     def __init__(self, subnet_choices, *args, **kwargs):
         super(CalculatorForm, self).__init__(*args, **kwargs)
         self.fields['subnet'].choices = subnet_choices
+
+    def clean_network_class(self):
+        network_class = self.cleaned_data.get('network_class')
+        if network_class == 'default':
+            raise forms.ValidationError("Please select network class")
+        return self.cleaned_data
