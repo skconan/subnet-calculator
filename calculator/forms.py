14d70a86ce91b79c48921132f5a56174505efb74
skconan <supakit.kr@gmail.com>
2017-11-21 02:59:51 -0800

    refactor code

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,17 +1,15 @@
 from django import forms
-from .subnet_calculator import *
 from django.core.validators import RegexValidator
 
-
+NETWORK_CLASS_CHOICES = (('default', 'Please Select a network class'), ('Any', 'Any'), ('A', 'A'), ('B', 'B'), ('C', 'C'),)
 class CalculatorForm(forms.Form):
-    network_class = forms.ChoiceField(choices=())
+    network_class = forms.ChoiceField(choices=NETWORK_CLASS_CHOICES)
     subnet = forms.ChoiceField(choices=())
     ip_address = forms.CharField(initial='0.0.0.0', validators=[RegexValidator(
         regex=r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
         message='IP Address is not a valid.'
     )])
 
-    def __init__(self, subnet_choices, class_choices, *args, **kwargs):
+    def __init__(self, subnet_choices, *args, **kwargs):
         super(CalculatorForm, self).__init__(*args, **kwargs)
         self.fields['subnet'].choices = subnet_choices
-        self.fields['network_class'].choices = class_choices
