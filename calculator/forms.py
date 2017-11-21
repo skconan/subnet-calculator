55c0403fe37e2c1cc6c9a150d3b54b5921d4f4d6
skconan <supakit.kr@gmail.com>
2017-11-21 01:26:00 -0800

    del attrs, init choices = Null, and refactor code

--- calculator/forms.py
+++ calculator/forms.py
@@ -2,15 +2,16 @@ from django import forms
 from .subnet_calculator import *
 from django.core.validators import RegexValidator
 
+
 class CalculatorForm(forms.Form):
-    CLASS_CHOICES = (('Any','Any'),('A','A'),('B','B'),('C','C'))
-    SUBNET_CHOICES = create_subnet('Any')
-    network_class = forms.ChoiceField(widget=forms.Select(attrs={'onchange': 'this.form.submit()'}), choices=CLASS_CHOICES)
-    subnet = forms.ChoiceField(choices=SUBNET_CHOICES)
-    ip_address = forms.CharField(initial='0.0.0.0',validators=[RegexValidator(
-            regex=r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
-            message='IP Address is not a valid.'
-        )])
-    def __init__(self, subnet_choices, *args, **kwargs):
+    network_class = forms.ChoiceField(choices=())
+    subnet = forms.ChoiceField(choices=())
+    ip_address = forms.CharField(initial='0.0.0.0', validators=[RegexValidator(
+        regex=r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
+        message='IP Address is not a valid.'
+    )])
+
+    def __init__(self, subnet_choices, class_choices, *args, **kwargs):
         super(CalculatorForm, self).__init__(*args, **kwargs)
         self.fields['subnet'].choices = subnet_choices
+        self.fields['network_class'].choices = class_choices
