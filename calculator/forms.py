e3378b275f626f3c7a85faff1d45330be88948f9
skconan <supakit.kr@gmail.com>
2017-11-21 06:35:56 -0800

    recheck and fix

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,24 +1,15 @@
 from django import forms
 from django.core.validators import RegexValidator
 
-NETWORK_CLASS_CHOICES = (('default', 'Please Select a network class'),
+NETWORK_CLASS_CHOICES = (('default', 'Please select a network class'),
                          ('Any', 'Any'), ('A', 'A'), ('B', 'B'), ('C', 'C'),)
 
 
 class CalculatorForm(forms.Form):
     network_class = forms.ChoiceField(choices=NETWORK_CLASS_CHOICES)
     subnet = forms.ChoiceField(choices=())
-    ip_address = forms.CharField(initial='0.0.0.0', validators=[RegexValidator(
-        regex=r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
-        message='IP Address is not a valid.'
-    )])
+    ip_address = forms.CharField(initial='0.0.0.0')
 
     def __init__(self, subnet_choices, *args, **kwargs):
         super(CalculatorForm, self).__init__(*args, **kwargs)
         self.fields['subnet'].choices = subnet_choices
-
-    def clean_network_class(self):
-        network_class = self.cleaned_data.get('network_class')
-        if network_class == 'default':
-            raise forms.ValidationError("Please select network class")
-        return self.cleaned_data
--- calculator/views.py
+++ calculator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import CalculatorForm
 from .subnet_calculation import *
 from django.http import HttpResponse
-
+import re
 
 def calculator(request):
     CD = CreateData()
@@ -16,24 +16,32 @@ def calculator(request):
         return HttpResponse(form)
 
     elif request.method == 'POST':
+        err_ip = False
+        err_network_class = False
+        
         network_class = request.POST['network_class']
-        subnet_choices = CD.get_subnet(network_class)
-        form = CalculatorForm(subnet_choices, request.POST)
-        if form.is_valid():
-            subnet_mask = request.POST['subnet']
-            ip_address = request.POST['ip_address']
+        subnet_mask = request.POST['subnet']
+        ip_address = request.POST['ip_address']
 
+        regex=r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"
+    
+        if re.match(regex,ip_address) is None:
+            err_ip = 'IP Address is not a valid.'
+        if network_class == 'default':
+            err_network_class = 'Please select a network class'
+            
+        if not (err_ip or err_network_class):
             SC = SubnetCalculation(network_class, subnet_mask, ip_address)
             data = SC.calculate()
-
             subnet_choices = CD.get_subnet('default')
             form = CalculatorForm(subnet_choices,
-                                  initial={'ip_address': ip_address})
+                                initial={'ip_address': ip_address})
             return render(request, 'calculator.html', {'form': form, 'data': data})
         else:
+            err = {'err_ip':err_ip,'err_network_class':err_network_class}
             subnet_choices = CD.get_subnet(network_class)
             form = CalculatorForm(subnet_choices, request.POST)
-            return render(request, 'calculator.html', {'form': form})
+            return render(request, 'calculator.html', {'form': form,'err':err})
     else:
         subnet_choices = CD.get_subnet('default')
         form = CalculatorForm(subnet_choices,
