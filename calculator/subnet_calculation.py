3c8b407c7ba152e0cf82801624aba267ad1f566b
skconan <supakit.kr@gmail.com>
2017-11-21 06:00:09 -0800

    rewrite

--- calculator/subnet_calculation.py
+++ calculator/subnet_calculation.py
@@ -19,12 +19,12 @@ class CreateData():
         subnet_tuple = ()
         subnet = 11111111111111111111111111111111
         subnet_str = self.subnet_bin2dec(subnet, 32)
-        subnet_tuple += ((subnet, subnet_str),)
+        subnet_tuple += ((subnet_str, subnet_str),)
 
         for i in range(0, 31):
             subnet -= pow(10, i)
             subnet_str = self.subnet_bin2dec(subnet, 31 - i)
-            subnet_tuple += ((subnet, subnet_str),)
+            subnet_tuple += ((subnet_str, subnet_str),)
             if 31 - i == 24:
                 subnet_mask['C'] = subnet_tuple
             elif 31 - i == 16:
@@ -34,99 +34,111 @@ class CreateData():
             elif 31 - i == 1:
                 subnet_mask['Any'] = subnet_tuple
         return subnet_mask
-        
+
     def get_subnet(self, network_class):
         return self.subnet_mask[network_class]
 
-# class SubnetCalculation():
-#     def __init__(self,subnet_mask,ip_address,network_class):
-#         self.network_class = network_class
-#         self.ip_address = ip_address
-#         self.subnet_mask = subnet_mask.split('/')[0]
-#         self.cidr_notation = '/' + subnet_mask.split('/')[1]
 
+class SubnetCalculation():
+    def __init__(self, network_class, subnet_mask, ip_address):
+        self.network_class = network_class
+        self.network_address_bin = None
+        self.ip_address = ip_address
+        self.subnet_mask = subnet_mask.split('/')[0]
+        self.wildcard_mask = self.get_wildcard_mask()
+        self.network_address = self.get_network_address()
+        self.broadcast_address = self.get_broadcast_address()
+        self.cidr_notation = subnet_mask.split('/')[1]
+        self.short = self.ip_address + ' ' + self.cidr_notation
+        self.bin_id = self.get_bin_id()
+        self.int_id = self.get_integer_id()
+        self.no_host = self.get_total_number_of_host()
 
-def ip_class_is(ip_address):
-    ip = int(ip_address.split('.')[0])
-    if 0 <= ip <= 127:
-        return 'A'
-    elif 128 <= ip <= 191:
-        return 'B'
-    elif 192 <= ip <= 223:
-        return 'C'
-    elif 224 <= ip <= 239:
-        return 'D'
-    else:
-        return 'E'
+    def calculate(self):
+        data = {}
+        data['ip_address'] = self.ip_address
+        data['network_address'] = self.network_address
+        data['host_ip_range'] = self.get_host_ip_range()
+        data['broadcast_address'] = self.broadcast_address
+        data['number_of_host'] = self.no_host
+        data['number_of_usable_host'] = max(self.no_host - 2, 0)
+        data['subnet_mask'] = self.subnet_mask
+        data['wildcard_mask'] = self.wildcard_mask
+        data['binary_subnet_mask'] = self.get_subnet_mask_bin()
+        data['ip_class'] = self.get_ip_class()
+        data['cidr_notation'] = '/' + self.cidr_notation
+        data['ip_type'] = self.get_ip_type()
+        data['short'] = self.short
+        data['binary_id'] = self.bin_id
+        data['integer_id'] = self.int_id
+        data['hex_id'] = self.get_hex_id()
+        return data
 
+    def get_subnet_mask_bin(self):
+        subnet_bin = ''
+        for ip in self.subnet_mask.split('.'):
+            subnet_bin += "{0:08b}".format(int(ip)) + '.'
+        return subnet_bin[0:-1]
 
-def subnet2wildcard(subnet_mask):
-    wildcard_mask = ''
-    for subnet in subnet_mask.split('.'):
-        wildcard_mask += str(255 - int(subnet)) + '.'
-    return wildcard_mask[0:-1]
+    def get_network_address(self):
+        result = ''
+        for (ip, subnet) in zip(self.ip_address.split('.'), self.subnet_mask.split('.')):
+            result += str(int(ip) & int(subnet)) + '.'
+        return result[0:-1]
 
+    def get_wildcard_mask(self):
+        wildcard_mask = ''
+        for subnet in self.subnet_mask.split('.'):
+            wildcard_mask += str(255 - int(subnet)) + '.'
+        return wildcard_mask[0:-1]
 
-def bin_subnet(subnet_mask):
-    binary_subnet_mask = ''
-    for subnet in subnet_mask.split('.'):
-        binary_subnet_mask += "{0:08b}".format(int(subnet)) + '.'
-    return binary_subnet_mask[0:-1]
+    def get_ip_class(self):
+        ip = int(self.ip_address.split('.')[0])
+        if 0 <= ip <= 127:
+            return 'A'
+        elif 128 <= ip <= 191:
+            return 'B'
+        elif 192 <= ip <= 223:
+            return 'C'
+        elif 224 <= ip <= 239:
+            return 'D'
+        else:
+            return 'E'
 
+    def get_ip_type(self):
+        ip = self.ip_address.split('.')
+        if ((int(ip[0]) == 172 and 16 <= int(ip[1]) <= 31)
+            or (int(ip[0]) == 192 and int(ip[1]) == 168)
+                or int(ip[0]) == 10):
+            return 'Private'
+        return 'Public'
 
-def ip2bin_id(ip_address):
-    binary_id = ''
-    for ip in ip_address.split('.'):
-        binary_id += "{0:08b}".format(int(ip))
-    return binary_id
+    def get_bin_id(self):
+        bin_id = ''
+        for ip in self.ip_address.split('.'):
+            bin_id += "{0:08b}".format(int(ip))
+        return bin_id
 
+    def get_integer_id(self):
+        return int(self.bin_id, 2)
 
-def bin_id2int_id(binary_id):
-    integer_id = int(binary_id, 2)
-    return integer_id
+    def get_hex_id(self):
+        return hex(self.int_id)
 
+    def get_total_number_of_host(self):
+        return pow(2, 32 - int(self.cidr_notation))
 
-def int_id2hex_id(integer_id):
-    hex_id = hex(integer_id)
-    return hex_id
+    def get_broadcast_address(self):
+        result = ''
+        for (ip, subnet) in zip(self.ip_address.split('.'), self.wildcard_mask.split('.')):
+            result += str(int(ip) | int(subnet)) + '.'
+        return result[0:-1]
 
-
-def masking(ip_address, subnet_mask):
-    result = ''
-    for (ip, subnet) in zip(ip_address.split('.'), subnet_mask.split('.')):
-        result += str(int(ip) & int(subnet)) + '.'
-    return result[0:-1]
-
-
-def number_of_host_is(cidr_notation):
-    return pow(2, 32 - int(cidr_notation))
-
-
-def broadcast_address_is(ip_address, wildcard_mask):
-    result = ''
-    for (ip, subnet) in zip(ip_address.split('.'), wildcard_mask.split('.')):
-        result += str(int(ip) | int(subnet)) + '.'
-    return result[0:-1]
-
-
-def host_ip_range_is(network_address, broadcast_address):
-    ip_min = network_address.split('.')
-    ip_max = broadcast_address.split('.')
-    ip_min[3] = str(int(ip_min[3]) + 1)
-    ip_max[3] = str(int(ip_max[3]) - 1)
-    if ip_min[3] == ip_max[3] or ip_min[3] == '256' or ip_max[3] == '-1':
-        return None
-    print(ip_min)
-    print(ip_max)
-    return ip_min[0] + '.' + ip_min[1] + '.' + ip_min[2] + '.' + ip_min[3] + ' - ' + ip_max[0] + '.' + ip_max[1] + '.' + ip_max[2] + '.' + ip_max[3]
-
-
-def ip_type_is(ip_address):
-    ip = ip_address.split('.')
-    if int(ip[0]) == 10:
-        return 'Private'
-    if int(ip[0]) == 172 and 16 <= int(ip[1]) <= 31:
-        return 'Private'
-    if int(ip[0]) == 192 and int(ip[1]) == 168:
-        return 'Private'
-    return 'Public'
+    def get_host_ip_range(self):
+        ip_min = self.network_address.split('.')
+        ip_max = self.broadcast_address.split('.')
+        ip_min[3] = str(int(ip_min[3]) + 1)
+        ip_max[3] = str(int(ip_max[3]) - 1)
+        if self.no_host <= 2:
+            return None
+        return ip_min[0] + '.' + ip_min[1] + '.' + ip_min[2] + '.' + ip_min[3] + ' - ' + ip_max[0] + '.' + ip_max[1] + '.' + ip_max[2] + '.' + ip_max[3]
--- calculator/views.py
+++ calculator/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import CalculatorForm
 from .subnet_calculation import *
 from django.http import HttpResponse
-# Create your views here.
+
 
 def calculator(request):
     CD = CreateData()
@@ -11,49 +11,31 @@ def calculator(request):
         subnet_choices = CD.get_subnet(network_class)
         form = CalculatorForm(subnet_choices)
         form = str(form)
-        form = form.split('</tr>')[1].split('<td>')[1].split('</td>')[0].replace('name="subnet"','name="subnet" class="form-control"')
+        form = form.split('</tr>')[1].split('<td>')[1].split('</td>')[0].replace(
+            'name="subnet"', 'name="subnet" class="form-control"')
         return HttpResponse(form)
-      
+
     elif request.method == 'POST':
         network_class = request.POST['network_class']
-        subnet_choices = CD.get_subnet('default')
-        form = CalculatorForm(subnet_choices,request.POST) 
+        subnet_choices = CD.get_subnet(network_class)
+        form = CalculatorForm(subnet_choices, request.POST)
         if form.is_valid():
-            # subnet_mask_req = request.POST['subnet']
-            # ip_address = request.POST['ip_address']
-            # subnet_mask = subnet_mask_req.split('/')[0]
-            # network_address = masking(ip_address, subnet_mask)
-            # wildcard_mask = subnet2wildcard(subnet_mask)
-            # broadcast_address = broadcast_address_is(ip_address, wildcard_mask)
-            # binary_subnet_mask = bin_subnet(subnet_mask)
-            # host_ip_range = host_ip_range_is(network_address ,broadcast_address)
-            # ip_class = ip_class_is(ip_address)
-            # cidr_notation = '/' + subnet_mask_req.split('/')[1]
-            # cidr_num = subnet_mask_req.split('/')[1]
-            # number_of_host = "{:,}".format(number_of_host_is(cidr_num))
-            # number_of_usable_host = "{:,}".format(
-            #     max(0, number_of_host_is(cidr_num) - 2))
-            # ip_type = ip_type_is(ip_address)
-            # short = ip_address + ' ' + cidr_notation
-            # binary_id = ip2bin_id(ip_address)
-            # integer_id = bin_id2int_id(binary_id)
-            # hex_id = int_id2hex_id(integer_id)
-            # data = {'ip_address': ip_address, 'network_address': network_address,
-            #         'host_ip_range': host_ip_range, 'broadcast_address': broadcast_address,
-            #         'number_of_host': number_of_host, 'number_of_usable_host': number_of_usable_host,
-            #         'subnet_mask': subnet_mask, 'wildcard_mask': wildcard_mask,
-            #         'binary_subnet_mask': binary_subnet_mask, 'ip_class': ip_class,
-            #         'cidr_notation': cidr_notation, 'ip_type': ip_type,
-            #         'short': short, 'binary_id': binary_id,
-            #         'integer_id': integer_id, 'hex_id': hex_id,
-            #         }
+            subnet_mask = request.POST['subnet']
+            ip_address = request.POST['ip_address']
+
+            SC = SubnetCalculation(network_class, subnet_mask, ip_address)
+            data = SC.calculate()
+
+            subnet_choices = CD.get_subnet('default')
+            form = CalculatorForm(subnet_choices,
+                                  initial={'ip_address': ip_address})
             return render(request, 'calculator.html', {'form': form, 'data': data})
         else:
             subnet_choices = CD.get_subnet(network_class)
             form = CalculatorForm(subnet_choices, request.POST)
             return render(request, 'calculator.html', {'form': form})
-    else:    
+    else:
         subnet_choices = CD.get_subnet('default')
-        form = CalculatorForm(subnet_choices,initial={'ip_address':'0.0.0.0'}) 
+        form = CalculatorForm(subnet_choices,
+                              initial={'ip_address': '0.0.0.0'})
         return render(request, 'calculator.html', {'form': form})
-
